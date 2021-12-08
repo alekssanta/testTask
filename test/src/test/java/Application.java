d5a9ec9eb5d720936bec031d44b921c0a64e35e0
Lapin Aleksandr <Aleksandr.Lapin@homecredit.ru>
2021-12-08 12:07:11 +0400

    add class Kotik, Application

---
+++ test/src/test/java/Application.java
@@ -0,0 +1,70 @@
+public class Application {
+    //    private static Kotik senya;
+//    private static Kotik barsik;
+    static char name; // имя кота
+    static int weight; // вес кота
+
+    public static void main(String[] args) {
+
+        Kotik cat1 = new Kotik() {
+            @Override
+            public void getName() {
+
+            }
+
+            @Override
+            public void getWeight() {
+
+            }
+
+            @Override
+            public void getMeow() {
+
+            }
+
+            @Override
+            public void getPrettiness() {
+
+            }
+
+            private char barsik;
+
+            void setBarsik(){
+                name = barsik;
+                weight = 5;
+            }
+        };
+
+        Kotik cat2 = new Kotik(){
+            @Override
+            public void getName() {
+
+            }
+
+            @Override
+            public void getWeight() {
+
+            }
+
+            @Override
+            public void getMeow() {
+
+            }
+
+            @Override
+            public void getPrettiness() {
+
+            }
+
+            public void setKotik(){
+                int prettiness;
+                String name;
+                int weigh;
+                String meow;
+            }
+
+        };
+    }
+
+}
+
---
+++ test/src/test/java/Kotik.java
@@ -0,0 +1,52 @@
+
+public abstract class Kotik<degreeSatietyCat> {
+    private int degreeSatietyCat;
+    private static int count;
+
+    public void setCount(int count) {
+        this.count = count;
+        count++;
+    }
+
+    public void play() {
+    }
+
+    public void sleep() {
+
+    }
+
+    public void chaseMouse() {
+    }
+
+    public void eat() {
+    }
+
+    public void cleaningTheAssOnTheFloor() {
+
+    }
+
+    public void getName() {
+    }
+    public void getWeight() {
+    }
+    public void getMeow() {
+    }
+    public void getPrettiness() {
+    }
+    public void liveAnotherDay() {
+        for (int i = 0; i < 24; i++) ;
+
+    }
+
+
+
+//    if (degreeSatietyCat < 0){
+//     if (!useEnergy()) return;
+
+
+}
+//        System.out.println("кот голоден");
+
+
+
+
