2648b24ba447969626c108ac564f460136dcfedc
Lapin Aleksandr <Aleksandr.Lapin@homecredit.ru>
2021-12-08 18:46:36 +0400

    add cases

--- test/src/test/java/Application.java
+++ test/src/test/java/Application.java
@@ -6,65 +6,13 @@ public class Application {
 
     public static void main(String[] args) {
 
-        Kotik cat1 = new Kotik() {
-            @Override
-            public void getName() {
+        Kotik cat1 = new Kotik();
+        cat1.liveAnotherDay((int) (Math.random()*5+1));
 
-            }
 
-            @Override
-            public void getWeight() {
-
-            }
-
-            @Override
-            public void getMeow() {
-
-            }
-
-            @Override
-            public void getPrettiness() {
-
-            }
-
-            private char barsik;
-
-            void setBarsik(){
-                name = barsik;
-                weight = 5;
-            }
-        };
-
-        Kotik cat2 = new Kotik(){
-            @Override
-            public void getName() {
-
-            }
-
-            @Override
-            public void getWeight() {
-
-            }
-
-            @Override
-            public void getMeow() {
-
-            }
-
-            @Override
-            public void getPrettiness() {
-
-            }
-
-            public void setKotik(){
-                int prettiness;
-                String name;
-                int weigh;
-                String meow;
-            }
 
         };
     }
 
-}
+
 
--- test/src/test/java/Kotik.java
+++ test/src/test/java/Kotik.java
@@ -1,50 +1,99 @@
 
-public abstract class Kotik<degreeSatietyCat> {
+public class Kotik {
     private int degreeSatietyCat;
-    private static int count;
+    private int count;
 
     public void setCount(int count) {
         this.count = count;
         count++;
     }
 
-    public void play() {
+    boolean play() {
+        if (degreeSatietyCat <= 0) {
+            System.out.println("не удалось выполнить play, хочу есть");
+            return false;
+        }
+        degreeSatietyCat--;
+        System.out.println("играю");
+        return true;
     }
 
-    public void sleep() {
-
+    private boolean useEnergy() {
+        return false;
     }
 
-    public void chaseMouse() {
+    public boolean sleep() {
+        if (degreeSatietyCat <= 0) {
+            System.out.println("не удалось выполнить sleep, хочу есть");
+            return false;
+        }
+        degreeSatietyCat--;
+        System.out.println("сплю");
+        return true;
     }
 
-    public void eat() {
+    public boolean chaseMouse() {
+        if (degreeSatietyCat <= 0) {
+            System.out.println("не удалось выполнить chaseMouse, хочу есть");
+            return false;
+        }
+        degreeSatietyCat--;
+        System.out.println("ловлю мышь");
+        return true;
     }
 
-    public void cleaningTheAssOnTheFloor() {
-
+    public boolean eat() {
+        return false;
     }
 
-    public void getName() {
-    }
-    public void getWeight() {
-    }
-    public void getMeow() {
-    }
-    public void getPrettiness() {
-    }
-    public void liveAnotherDay() {
-        for (int i = 0; i < 24; i++) ;
-
+    public boolean cleaningTheAssOnTheFloor() {
+        if (degreeSatietyCat <= 0) {
+            System.out.println("не удалось выполнить cleaningTheAssOnTheFloor, хочу есть");
+            return false;
+        }
+        degreeSatietyCat--;
+        System.out.println("чищу об пол одно место");
+        return true;
     }
 
+    public boolean run() {
+        if (degreeSatietyCat <= 0) {
+            System.out.println("не удалось выполнить run, хочу есть");
+            return false;
+        }
+        degreeSatietyCat--;
+        System.out.println("бегаю");
+        return true;
+    }
+    public void liveAnotherDay(int randomNum) {
+        for (int i = 0; i < 24; i++);
+        switch (randomNum) {
+            case (1):
+                this.run();
+                break;
+            case (2):
+                this.play();
+                break;
+            case (3):
+                this.chaseMouse();
+                break;
+            case (4):
+                this.cleaningTheAssOnTheFloor();
+                break;
+            case (5):
+                this.sleep();
+                break;
+
+            default:
+                throw new IllegalStateException("Unexpected value: " + 1);
+        }
 
 
-//    if (degreeSatietyCat < 0){
-//     if (!useEnergy()) return;
+    }}
 
 
-}
+
+
 //        System.out.println("кот голоден");
 
 
